966d68983211d0656e72f86a82ba0f9895eefbbc
Nikita <nikitatimoshin23@gmail.com>
2023-09-26 00:29:37 +0300

    feat: implemented basic layout & sidebars

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,8 +2,12 @@ import { ClerkProvider } from '@clerk/nextjs';
 import '../globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import TopBar from '@/components/shared/TopBar';
+import LeftSideBar from '@/components/shared/LeftSideBar';
+import RightSideBar from '@/components/shared/RightSideBar';
+import BottomBar from '@/components/shared/BottomBar';
 
-const inter = Inter({ subsets: ['latin'] });
+const font = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Threads',
@@ -18,7 +22,17 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={font.className}>
+          <TopBar />
+          <main className="flex flex-row">
+            <LeftSideBar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+            <RightSideBar />
+          </main>
+          <BottomBar />
+        </body>
       </html>
     </ClerkProvider>
   );
